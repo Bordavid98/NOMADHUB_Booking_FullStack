bee113c0899f153345d081833d5ab269887dc3ce
Bordavid98 <bordavid98@gmail.com>
2024-07-26 18:46:28 +0200

    chore: Update ImageUpload component to use onSuccess instead of onUpload

--- app/components/inputs/ImageUpload.tsx
+++ app/components/inputs/ImageUpload.tsx
@@ -6,72 +6,72 @@ import { useCallback } from "react";
 import { TbPhotoPlus } from "react-icons/tb";
 
 declare global {
-	var cloudinary: any;
+    var cloudinary: any;
 }
 
 interface ImageUploadProps {
-	onChange: (value: string) => void;
-	value: string;
+    onChange: (value: string) => void;
+    value: string;
 }
 
 const ImageUpload: React.FC<ImageUploadProps> = ({
-	onChange,
-	value
+    onChange,
+    value
 }) => {
-	const handleUpload = useCallback((result: any) => {
-		onChange(result.info.secure_url);
-	}, [onChange]);	
+    const handleUpload = useCallback((result: any) => {
+        onChange(result.info.secure_url);
+    }, [onChange]);    
 
-	return ( 
-		<CldUploadWidget
-			onUpload={handleUpload}
-			uploadPreset="dguoxeu4"
-			options={{
-				maxFiles: 1,
-			}}
-		>
-			{({ open }) => {
-				return (
-					<div
-						onClick={() => open?.()}
-						className="
-							relative
-							cursor-pointer
-							hover:opacity-70
-							transition
-							border-dashed
-							border-2
-							p-20
-							border-neutral-300
-							flex
-							flex-col
-							justify-center
-							items-center
-							gap-4
-							text-neutral-600
-						"
-					>
-						<TbPhotoPlus size={50}/>
-						<div className="font-bold text-lg">
-							Click to upload
-						</div>
-						{value && (
-							<div
-								className="absolute inset-0 w-full h-full"
-							>	
-								<Image
-									alt="Upload"
-									fill
-									style={{ objectFit: "cover" }}
-									src={value}
-								/>
-							</div>
-						)}
-					</div>
-				);
-			}}
-		</CldUploadWidget>
-	 );
+    return ( 
+        <CldUploadWidget
+            onSuccess={handleUpload}
+            uploadPreset="dguoxeu4"
+            options={{
+                maxFiles: 1,
+            }}
+        >
+            {({ open }) => {
+                return (
+                    <div
+                        onClick={() => open?.()}
+                        className="
+                            relative
+                            cursor-pointer
+                            hover:opacity-70
+                            transition
+                            border-dashed
+                            border-2
+                            p-20
+                            border-neutral-300
+                            flex
+                            flex-col
+                            justify-center
+                            items-center
+                            gap-4
+                            text-neutral-600
+                        "
+                    >
+                        <TbPhotoPlus size={50}/>
+                        <div className="font-bold text-lg">
+                            Click to upload
+                        </div>
+                        {value && (
+                            <div
+                                className="absolute inset-0 w-full h-full"
+                            >    
+                                <Image
+                                    alt="Upload"
+                                    fill
+                                    style={{ objectFit: "cover" }}
+                                    src={value}
+                                />
+                            </div>
+                        )}
+                    </div>
+                );
+            }}
+        </CldUploadWidget>
+     );
 }
- 
+
 export default ImageUpload;
